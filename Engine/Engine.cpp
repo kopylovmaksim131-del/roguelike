3985ecf15f05e2d0442589640bdff75b5e6fe7b5
Maksim Kopylov <kopylovmaksim131@gmail.com>
2026-07-09 21:46:27 +0300

    start to do roguelike

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "Engine.h"
-//#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 Engine::Engine() {}
@@ -13,4 +13,19 @@ void Engine::Initialization()
 void Engine::Run()
 {
 	std::cout << "Engine run" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test SFML from Engine");
+
+   while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
 }
